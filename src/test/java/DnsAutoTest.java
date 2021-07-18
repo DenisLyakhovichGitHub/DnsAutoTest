f9e23c67ed6960f827db27305881eb6829ca47b8
Denis Lyakhovich <>
2021-07-18 18:05:59 +0300

    no message

--- src/test/java/DnsAutoTest.java
+++ src/test/java/DnsAutoTest.java
@@ -37,30 +37,45 @@ public class DnsAutoTest {
     driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
     driver.manage().window().maximize();
     logger.info("Открыта страница Yandex - " + "https://www.dns-shop.ru");
+    logger.info("--------------------------------------");
 
     //2.вывод заголовка страцицы
     String title = driver.getTitle();
     logger.info("Title - " + title);
+    logger.info("--------------------------------------");
 
     //3.вывод URL страницы
     String currentURL = driver.getCurrentUrl();
     logger.info("CurrentURL - " + currentURL);
+    logger.info("--------------------------------------");
 
     //4.Переход по ссылке бытовая техника
     WebElement elementPopAppLocation = driver.findElement(By.linkText("Да"));
     logger.info("WebElement: " + elementPopAppLocation.getText());
     elementPopAppLocation.click();
+    logger.info("--------------------------------------");
     WebElement elementAppliances = driver.findElement(By.linkText("Бытовая техника"));
     logger.info("WebElement: " + elementAppliances.getText());
     elementAppliances.click();
+    logger.info("--------------------------------------");
 
     //5.Вывод всех категорий товаров бытовой техники в логи
     List<WebElement> elements = driver.findElements(By.className("subcategory__title"));
     for(WebElement element : elements)
       logger.info("WebElement: " + element.getText());
+    logger.info("--------------------------------------");
 
     //6.Добавление и вывод Cookie
-    logger.info("Куки от Yandex");
+    logger.info("Куки, которое добавили мы");
+    driver.manage().addCookie(new Cookie("Cookie 1", "This Is Cookie 1"));
+    Cookie cookie1  = driver.manage().getCookieNamed("Cookie 1");
+    logger.info(String.format("Domain: %s", cookie1.getDomain()));
+    logger.info(String.format("Expiry: %s",cookie1.getExpiry()));
+    logger.info(String.format("Name: %s",cookie1.getName()));
+    logger.info(String.format("Path: %s",cookie1.getPath()));
+    logger.info(String.format("Value: %s",cookie1.getValue()));
+    logger.info("--------------------------------------");
+    logger.info("Куки от DNS");
     Set<Cookie> cookies = driver.manage().getCookies();
     for(Cookie cookie : cookies) {
       logger.info(String.format("Domain: %s", cookie.getDomain()));
--- src/test/java/WebDriverFactory.java
+++ src/test/java/WebDriverFactory.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import java.lang.*;
 
 
 public class WebDriverFactory {
@@ -16,35 +17,37 @@ public class WebDriverFactory {
 
   public static WebDriver getDriver(String browserName,String pageLoadStrategy) {
 
-    switch (browserName) {
+    if (pageLoadStrategy.equals("NORMAL")|| pageLoadStrategy.equals("EAGER") || pageLoadStrategy.equals("NONE")){
 
-      case "chrome":
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions optionsChrome = new ChromeOptions();
-        //специфические настройки браузера
-        optionsChrome.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
-        optionsChrome.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, false);
-        optionsChrome.setPageLoadStrategy(PageLoadStrategy.fromString(pageLoadStrategy));
-        optionsChrome.addArguments("--incognito");
+      switch (browserName) {
+        case "chrome":
+          WebDriverManager.chromedriver().setup();
+          ChromeOptions optionsChrome = new ChromeOptions();
+          //специфические настройки браузера
+          optionsChrome.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
+          optionsChrome.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, false);
+          optionsChrome.setPageLoadStrategy(PageLoadStrategy.fromString(pageLoadStrategy));
+          optionsChrome.addArguments("--incognito");
+          logger.info("Драйвер для браузера Chrome");
+          return new ChromeDriver(optionsChrome);
+        case "firefox":
+          WebDriverManager.firefoxdriver().setup();
+          FirefoxOptions optionsFireFox = new FirefoxOptions();
+          //специфические настройки браузера
+          optionsFireFox.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
+          optionsFireFox.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, false);
+          optionsFireFox.setPageLoadStrategy(PageLoadStrategy.fromString(pageLoadStrategy));
+          optionsFireFox.addArguments("-private");
+          logger.info("Драйвер для браузера Firefox");
+          return new FirefoxDriver(optionsFireFox);
+        default:
+          throw new RuntimeException("Incorrect browser name");
+      }
 
-        logger.info("Драйвер для браузера Chrome");
-        return new ChromeDriver(optionsChrome);
-
-      case "firefox":
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions optionsFireFox = new FirefoxOptions();
-        //специфические настройки браузера
-        optionsFireFox.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.DISMISS);
-        optionsFireFox.setCapability(CapabilityType.SUPPORTS_JAVASCRIPT, false);
-        optionsFireFox.setPageLoadStrategy(PageLoadStrategy.fromString(pageLoadStrategy));
-        optionsFireFox.addArguments("--incognito");
-
-        logger.info("Драйвер для браузера Firefox");
-        return new FirefoxDriver(optionsFireFox);
-
-      default:
-        throw new RuntimeException("Incorrect browser name");
+   } else {
+      throw new RuntimeException("Incorrect page load strategy");
     }
+
   }
 }
 
